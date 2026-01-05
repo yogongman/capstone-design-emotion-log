9e0c9ec80f5ead138911cbc83d984b449fce6d89
yogongman <yogongman@gmail.com>
2026-01-05 16:57:58 +0900

    cors 모든 ip 허용

--- backend/src/main/java/com/team/backend/config/WebConfig.java
+++ backend/src/main/java/com/team/backend/config/WebConfig.java
@@ -6,11 +6,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:3001")
+                .allowedOriginPatterns("*") // [변경] 모든 IP 주소에서 오는 요청 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowCredentials(true);
+                .allowCredentials(true); // 쿠키/인증 정보 포함 허용
     }
 }
